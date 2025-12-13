365c356bcc9d1c6449ccb405ae5b524d1a54b4a7
sadesuu <hsolistorrijos@gmail.com>
2025-12-13 12:53:35 +0100

    Añadido nuevo contenido y archivos de documentación para las clases y paquetes del proyecto.

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"App"},{"l":"Controller"},{"l":"Model.Entities"},{"l":"Model.Enum"},{"l":"View"}];updateSearchResults();
